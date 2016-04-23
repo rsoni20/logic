bca135eaf160bb426a09019046726c7974e2f0dd
rsoni20 <ravijsoni@outlook.com>
2016-04-23 15:27:03 +0530

    New logic

--- reverse/reverse.js
+++ reverse/reverse.js
@@ -1,56 +1,25 @@
-list = ["ravij", "bhuwaneshwari", "bhanuj", "jayendra", "baby"];
-
-
-
-var newList = [];
-
-
-function reverse() {
-
-    for (var counter = 4; counter >= 0; counter--) {
-        newList = list[counter];
-
-
-        document.write(newList);
-
-    }
-
-
-
-}
-
-
-
-
-function check() {
-    var element = document.getElementById('check').value;
-
-    for (var i = 0; i < list.length; i++) {
-
-
-        if (element == list[i]) {
-
-            var availability = true;
-
-            break;
-
+function operation() {
+    var x = document.getElementById("textArray").value;
+    var spl = x.split(",");
+    console.log(spl);
+    var newList = reverse(spl);
+    console.log(newList);
+    // Write a function that returns the elements on odd positions in a list.
+    var oddList = [];
+    for (var i = 0; i < newList.length; i++) {
+        if (i % 2 != 0) {
+            oddList.push(newList[i]);
         }
-
-
-        else {
-
-            availability = false;
-
-        }
-        
-        
-        
     }
-    
-  
-
-
-
+    console.log(oddList);
+    console.log(oddList.length);
 }
-
-
+function reverse(str) {
+    var newList = [];
+    for (var counter = 0; counter < str.length; counter++) {
+        if (str[counter].trim() != "") {
+            newList.unshift(str[counter].trim());
+        }
+    }
+    return newList;
+}
